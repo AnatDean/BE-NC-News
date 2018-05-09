6a4b9f72148a96781b36b91d262c95dc83045b01
AnatDean <32910752+AnatDean@users.noreply.github.com>
2018-05-09 16:20:05 +0100

    Seed test (#4)
    * implemented app and api routes architecture
    
    * abstracted seed function for testing and implemented test suite for seed function
    
    * removed some consolelogs

--- seed/seed.js
+++ seed/seed.js
@@ -1,60 +1,9 @@
 env = process.env.NODE_ENV;
-const {Topics, Users, Articles, Comments} = require('../models/index');
 const {topicData, userData, articleData}  = require(`./${env}Data/`);
+const {seedDB} = require('./seedDB');
+const {DB, PORT} = require('../config/index');
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
-const {DB, PORT} = require('../config/index');
-const Chance = require('chance');
-const chance = new Chance;
-
-const seedDB = (DB, topics, users, articles) => {
-    let topicIds, userIds, articleIds
-        return Promise.all([Topics.insertMany(topics), Users.insertMany(users)])
-    .then(([topics, users]) => {
-        console.log(`inserted ${topics.length} topics` )
-        console.log(`inserted ${users.length} users` )
-        topicIds = topics;
-        userIds = users;
-        const newArticles = articles.map((article, i) => {
-            article.belongs_to = topicIds.find(topic => topic.slug === article.topic)._id;
-            article.votes = Math.floor(Math.random() * 50);
-            article.created_by = userIds[Math.floor(Math.random() * (userIds.length))]._id;
-            return article
-        });
-        return Articles.insertMany(newArticles)
-    })
-    .then(articles => {
-        console.log(`inserted ${articles.length} articles` )
-        articleIds = articles;
-        const comments = [];
-
-        articleIds.forEach(article => {
-            let randomCallCount = Math.floor(Math.random() * 5)
-            while (randomCallCount){
-                comments.push(createComment(article._id));
-                randomCallCount--;
-            }
-        })
-        function createComment (articleId) {
-            return comment = {
-                body: chance.sentence(),
-                belongs_to: articleId,
-                created_at: chance.timestamp(),
-                votes: Math.floor(Math.random() * 50),
-                created_by: userIds[Math.floor(Math.random() * (userIds.length))]._id
-            };
-        }
-        return Comments.insertMany(comments)
-    })
-    .then(comments => {
-        console.log(`inserted ${comments.length} comments`)
-        console.log('finished seeding!')
-        mongoose.disconnect()
-    })
-    .catch(err => {
-        console.log(err);
-    })
-}
 
 mongoose.connect(DB)
 .then(() => {
@@ -67,5 +16,4 @@ mongoose.connect(DB)
 .then(() => {
     mongoose.disconnect()
 })
-
-module.exports = {seedDB}
+.catch(err => console.log('seedfile:', err))
