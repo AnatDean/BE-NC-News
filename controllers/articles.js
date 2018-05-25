6353adc442b5797150cc515c10c9f00cfcd9f7dd
anatdean <anat.dean@hotmail.com>
2018-05-25 11:49:35 +0100

    adjusted structure of addcomment return value

--- controllers/articles.js
+++ controllers/articles.js
@@ -52,8 +52,8 @@ exports.addComment = (req,res,next) => {
         return Comments.create(comment)
     })
     .then(({_doc}) => {
-        const sendComment = {..._doc, created_by: 'northcoder' }
-        res.status(201).send({comment:sendComment}) 
+        _doc.created_by.username = 'northcoder' ;
+        res.status(201).send({comment:_doc}) 
     })
     .catch(err => {
         if (err.name ==='ValidationError' || err.name === 'CastError') return next({status:404, controller: "article"})
